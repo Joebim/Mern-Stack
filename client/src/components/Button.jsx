a1cc56c6dcef76fe7c590bb2ec77789c7d1ef2b5
Joebim <josey359@gmail.com>
2024-04-22 13:51:21 +0100

    [FIX] fetch issue

--- client/src/components/Button.jsx
+++
@@ -1,3 +0,0 @@
-export const Button = (props) => {
-    return <button className={`px-[15px] py-[5px] duration-200 font-semibold rounded-[5px] text-[13px] hover:bg-gray-100 ${props.className}`}>{props.children}</button>
-}
